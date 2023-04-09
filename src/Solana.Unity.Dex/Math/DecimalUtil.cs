0fb5bc72d22b10c6fe32e261e7e2c2e04de25dfd
Gabriele Picco <piccogabriele@gmail.com>
2023-04-09 20:25:32 +0200

    :bug: Switch from double to decimal type (#28)
    Switch from double to decimal type to handle edge cases

--- src/Solana.Unity.Dex/Math/DecimalUtil.cs
+++ src/Solana.Unity.Dex/Math/DecimalUtil.cs
@@ -4,23 +4,28 @@ namespace Solana.Unity.Dex.Math;
 
 public class DecimalUtil
 {
-    public static double FromUlong(ulong value, int shift = 0)
+    public static decimal FromUlong(ulong value, int shift = 0)
     {
-        return value / System.Math.Pow(10, shift);
+        return value / (decimal)System.Math.Pow(10, shift);
     }
     
-    public static double FromBigInteger(BigInteger value, int shift = 0)
+    public static decimal FromBigInteger(BigInteger value, int shift = 0)
     {
         return FromUlong((ulong)value, shift);
     }
     
     public static ulong ToUlong(double value, int shift = 0)
     {
-        return (ulong)(value * System.Math.Pow(10, shift));
+        return ToUlong((decimal)value, shift);
     }
     
     public static ulong ToUlong(float value, int shift = 0)
     {
-        return ToUlong((double)value, shift);
+        return ToUlong((decimal)value, shift);
+    }
+    
+    public static ulong ToUlong(decimal value, int shift = 0)
+    {
+        return (ulong)(value * (decimal)System.Math.Pow(10, shift));;
     }
 }
